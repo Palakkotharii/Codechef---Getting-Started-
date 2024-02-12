3a7d035f16d49e158efd8f574df0197613d34587
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-02-12 12:22:52 +0530

    contest added

---
+++ Biryani classes.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        int y;
+        cin >> x >> y;
+        cout << x * y << endl;
+    }
+}
