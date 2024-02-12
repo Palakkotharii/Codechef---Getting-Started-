e200c4d32a2bc022fba696f7abf49e269a4f9a22
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-02-12 12:23:07 +0530

    contest added

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if (x == 6)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+}
