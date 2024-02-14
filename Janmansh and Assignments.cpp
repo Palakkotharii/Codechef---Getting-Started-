675da3711c862045e7e5f57e6edf92c393bf836b
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-02-14 23:19:48 +0530

    contest practice added

---
+++ Janmansh and Assignments.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if ((x + 3) == 10)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+}
