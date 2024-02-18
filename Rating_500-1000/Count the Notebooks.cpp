619a784b16e19bfabe4ae37d33987f9883b283ab
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-02-19 00:19:34 +0530

    contest practice added

---
+++ Rating_500-1000/Count the Notebooks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int ans = n * 1000;
+        cout << ans / 100 << endl;
+    }
+}
