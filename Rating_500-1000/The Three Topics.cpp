fbad9d205f41666ed590de917edec5364de686b2
Palak <89315491+Palakkotharii@users.noreply.github.com>
2024-02-19 12:44:34 +0530

    practice added

---
+++ Rating_500-1000/The Three Topics.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int A, B, C, X;
+    cin >> A >> B >> C >> X;
+    if (A == X || B == X || C == X)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
